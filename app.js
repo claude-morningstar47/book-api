b02405b869b13f190a1ff91d521e71a971eea23c
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-04-01 18:59:32 +0000

    valide

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
-// redis
-const redis = require("redis");
-const redisStore = require("connect-redis")(session);
-const client = redis.createClient();
 require("dotenv").config({ path: "./config.env" });
 
 // Initialisation de l'application Express
@@ -26,7 +22,6 @@ app
   .use(express.urlencoded({ extended: true }));
 
 const db = require("./src/models");
-const { json } = require("body-parser");
 
 db.sequelize
   .sync({ force: false })
@@ -37,25 +32,7 @@ db.sequelize
     console.log("Failed to sync db: " + err.message);
   });
 
-// Use Redis as session store
-app.use(
-  session({
-    store: new redisStore({ client }),
-    secret: "your-secret-key",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-app.get("/", (req, res) => {
-  req.session.username = "john";
-  res.send("Welcome to book-api application!\n");
-});
-
-app.get("/user", (req, res) => {
-  const username = req.session.username;
-  res.send(`Session data retrieved successfully: ${username}`);
-});
+//
 
 // Routes (endpoints)
 require("./src/routes/user.routes")(app);
